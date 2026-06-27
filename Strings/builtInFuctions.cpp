11f7e9daacad42c8db1ac383e145edbd55a13b4f
Mayank-04423 <mayanktiwari0026@gmail.com>
2026-06-27 23:37:24 +0530

    using functions like push_back, pop_back , reverse entire string, reverse a part

---
+++ Strings/builtInFuctions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main() 
+{
+    // string str = "mississipi";
+
+    // cout<<str.length();     //o to str.length() - 1
+
+
+    // string str = "abcde";
+    // cout<<str<<"\n";
+    // str.push_back('f');
+    // str.push_back('g');
+    // cout<<str<<"\n";
+    // str.pop_back();
+    // cout<<str;
+
+    // string s = "abc";
+    // cout<<s<<"\n";
+    // s = s+"def"; //add kr sakte hai aage ya piche se place krke
+    // cout<<s;
+
+
+    string str = "abcdef";  //0,1,2,3,4,5
+    cout<<str<<"\n";
+    // reverse(str.begin(),str.end());
+    // cout<<str<<"\n";
+    reverse(str.begin()+2, str.begin()+5);  //cde ko everse krna hai jo 2nd se 4th tk hai  
+    cout<<str;
+}
