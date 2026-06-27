cd16dbf90a458d319e3e11a1a833e21c9ba064b5
Mayank-04423 <mayanktiwari0026@gmail.com>
2026-06-27 22:37:19 +0530

    getting started with string operations

---
+++ 2d array/IntroStrings.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char str[]={'a','b','\0','c','d','e'};
+
+    // for(int i=0;str[i]!='\0';i++){
+    //     cout<<str[i]<<" ";
+    // }
+
+    cout<<str;
+
+    // char str[5] = "abcd";
+    // for(int i=0; i<5; i++){
+    //     cout<<str[i]<<" ";
+    // }
+    // cout<<"\n";
+    // cout<<(int)(str[5]);    // 0 means that the string ends with a 5th index as \0 which wont be shown .
+}
