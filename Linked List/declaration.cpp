6e1da2cd19f364334da332014683c2075b8150e6
Mayank-04423 <mayanktiwari0026@gmail.com>
2026-07-03 13:00:56 +0530

    getting started with linked list . declaration and priniting

---
+++ Linked List/declaration.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node*next ;
+    Node(int val){
+        data = val;
+        next = nullptr;
+    }
+};
+
+Node* first = nullptr;
+//creating first node using 'new'
+void create(int A[],int n){
+    first= new Node(A[0]);
+    Node* last = first;
+    
+    for(int i=1; i<n;i++){
+        Node* t = new Node(A[i]);
+        last -> next = t;   //linking to new node 
+        last = t;           //moving the tracker forward
+    }
+}
+
+void disply(struct Node* temp){
+    while(temp!=nullptr){
+        cout<<temp->data<<"->";
+        temp= temp->next;
+    }
+
+}
+int main() 
+{
+    int A[]={2,3,3,4,40,5};
+    create(A,6);
+    disply(first);
+    return 0;
+}
