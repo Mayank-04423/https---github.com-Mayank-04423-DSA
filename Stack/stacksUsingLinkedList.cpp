a3ad49e2d176edf7618ac8dd0ee03b5af9be9eac
Mayank-04423 <mayanktiwari0026@gmail.com>
2026-07-07 23:15:20 +0530

    declaration

---
+++ Stack/stacksUsingLinkedList.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+
+    //constructor
+    Node(int n){
+        data = n;
+        next = nullptr;
+    }
+};
+
+int main() 
+{
+    
+    return 0;
+}
