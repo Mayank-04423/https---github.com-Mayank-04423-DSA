1e36bee4ec188b683a2619f0dcd73a7f19f79aea
Mayank-04423 <mayanktiwari0026@gmail.com>
2026-07-07 23:29:43 +0530

    push,pop,peek,display in stack using Linked List

--- Stack/stacksUsingLinkedList.cpp
+++ Stack/stacksUsingLinkedList.cpp
@@ -11,10 +11,78 @@ public:
         data = n;
         next = nullptr;
     }
+};    
+
+
+class stack{
+    Node* top;
+    public:
+    stack(){
+        top = nullptr;
+    }
+
+    //push (insert at head)
+    void push(int n){
+        Node* newNode = new Node(n);
+        newNode->next = top;
+        top = newNode;
+
+    }
+
+    //pop (delete the head)
+    void pop(){
+        if(top == nullptr){
+            cout<<"stack underflow"<<"\n";
+        }
+        Node* temp = top;
+        top = top -> next;
+        delete temp;
+    }
+
+    //peek
+    void peek(){
+        if(top == nullptr){
+            cout<<"stack underflow"<<"\n";
+        }
+        cout<<"The topmost element is : "<<top->data;
+    }
+
+    //display
+    void display(){
+         if(top == nullptr){
+            cout<<"stack is empty"<<"\n";
+        }
+        Node* temp = top;
+        while(temp!=0){
+            cout<<temp->data<<" -> ";
+            temp = temp->next;
+        }
+        cout<<"null";
+    }
+
+    //deconstructor 
+    ~stack(){
+        while(top!=nullptr){
+            pop();
+        }
+    }
 };
 
 int main() 
 {
-    
+    stack st;
+    st.push(10);
+    st.push(12);
+    st.push(32);
+    st.push(9);
+    st.push(14);
+    st.push(22);
+    st.display();
+    cout<<"\n";
+    st.peek();
+    cout<<"\n";
+
+    st.pop();
+    st.peek();
     return 0;
 }
